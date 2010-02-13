ed48b7d765cd4ab38299367a12b294e58b572ad4
Soren Dossing <sauber@localhost.(none)>
2010-02-13 17:48:55 +0900

    Near Strategy

---
+++ runtest.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd perl
+perl -c MyTronBot.pl
+java -jar engine/Tron.jar maps/empty-room.txt "perl MyTronBot.pl" "java -jar example_bots/RandomBot.jar"
+#java -jar engine/Tron.jar maps/empty-room.txt "perl MyTronBot.pl" "java -jar example_bots/Chaser.jar"
