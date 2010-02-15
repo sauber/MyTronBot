e1f573d5ff203f8b83d181cb557445b4d5ccc9fe
Soren Dossing <sauber@localhost.(none)>
2010-02-15 19:42:04 +0900

    Die on compile failure

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd perl
-perl -c MyTronBot.pl
+perl -c MyTronBot.pl || exit
 #java -jar engine/Tron.jar maps/empty-room.txt "perl MyTronBot.pl" "java -jar example_bots/RandomBot.jar"
 java -jar engine/Tron.jar maps/ring.txt "perl MyTronBot.pl" "java -jar example_bots/RandomBot.jar"
 #java -jar engine/Tron.jar maps/empty-room.txt "perl MyTronBot.pl" "java -jar example_bots/Chaser.jar"
